699edf035850fa9c11179a2355b5f2b9cdc2904c
milosmns <milosmns@gmail.com>
2020-07-26 17:05:56 +0200

    Add a (manual) DI container

---
+++ Sources/App/DI/DI.swift
@@ -0,0 +1,62 @@
+import Foundation
+import Vapor
+
+final class DI {
+    
+    private static var container: DI? = nil
+    
+    static func configure(
+        _ cacheConfig: CacheConfig,
+        _ persistenceConfig: PersistenceConfig,
+        _ timeProvider: TimeProvider
+    ) throws {
+        if (container != nil) { throw "Container already initialized" }
+        container = DI(cacheConfig, persistenceConfig, timeProvider)
+    }
+    
+    static func shared() -> DI { return container! }
+    
+    // Container properties
+    
+    private let cacheConfig: CacheConfig
+    private let persistenceConfig: PersistenceConfig
+    private let timeProvider: TimeProvider
+    private let lock = DispatchSemaphore(value: 1)
+    
+    private lazy var accountCache: IAccountCache = { return AccountInMemCache(maxSize: cacheConfig.maxStoredItems) }()
+    
+    private lazy var accountMemPersistence: IAccountPersistence = { return AccountPersistenceInMem(timeProvider: timeProvider) }()
+    
+    // Initializer
+    
+    private init(
+        _ cacheConfig: CacheConfig,
+        _ persistenceConfig: PersistenceConfig,
+        _ timeProvider: TimeProvider
+    ) {
+        self.cacheConfig = cacheConfig
+        self.persistenceConfig = persistenceConfig
+        self.timeProvider = timeProvider
+    }
+    
+    // Cache
+    
+    func getAccountCache() -> IAccountCache {
+        lock.wait()
+        defer { lock.signal() }
+        return accountCache
+    }
+    
+    // Persistence
+    
+    func getAccountPersistence(for request: Request) -> IAccountPersistence {
+        lock.wait()
+        defer { lock.signal() }
+        switch persistenceConfig.type {
+            // SQL works with a request, so a new instance is required every time
+            case .mysql: return AccountPersistenceMySQL(timeProvider: timeProvider, request: request)
+            case .inmem: return accountMemPersistence
+        }
+    }
+    
+}
